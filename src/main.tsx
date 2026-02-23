4ab3b4104c032d775d83cae251cafc4a5ea0e7f3
tishshii <cadutemisteriose@gmail.com>
2026-02-23 16:43:22 +0400

    integrate tg

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,10 @@ import { TouchBackend } from 'react-dnd-touch-backend'
 import App from './App'
 import './index.css'
 
+const tg = (window as any).Telegram?.WebApp;
+tg?.expand?.();
+tg?.disableVerticalSwipes?.();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
