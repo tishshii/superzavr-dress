286698ca719be6d206ae0114ca9aff816a55dfbd
tishshii <cadutemisteriose@gmail.com>
2026-02-23 14:29:48 +0400

    pages

--- src/usePixelHitTest.ts
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react'
-import { ITEM_CATALOG } from './config'
-import { preloadAllItems, isOpaqueAt } from './hitTest'
-
-export function usePixelHitTest() {
-  useEffect(() => { preloadAllItems(ITEM_CATALOG) }, [])
-  return { isOpaqueAt }
-}
